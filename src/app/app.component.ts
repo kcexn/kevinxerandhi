91b4be7c448a04c266e81feb2b6d7cd85e5eea3c
Kevin Exton <068ant@gmail.com>
2019-10-24 21:48:22 +1100

    Made some progress on the navbar toggling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  collapsed = false;
+  collapsed = true;
+
+  toggleCollapse() {
+    this.collapsed = !this.collapsed;
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -14,6 +15,7 @@ import { InvitationComponent } from './invitation/invitation.component';
   imports: [
     NgbModule,
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
