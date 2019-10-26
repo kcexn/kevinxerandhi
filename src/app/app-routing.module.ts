8d0f25a81016f708fbf80e9f2df3d458fa7ef239
Kevin Exton <068ant@gmail.com>
2019-10-26 17:55:27 +1100

    So turns out ngx-bootstrap is also missing features. So I did some more searching today and found angular-bootstrap-md, it's more complicated than ngx-bootstrap and actually replaces the Bootstrap css entirely within itself but it has ALL of the features I need. I'm using the free tier. So I did another migration from ngx-bootstrap to ng-bootstrap and now both the navbar AND the carousel work. Unfortunately it changed all of my default fonts, but for the time being that is OK. I need to change the fonts again later anyway. In addition I added in a new location component where we can fill in details about Freycinet Lodge.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InvitationComponent } from './invitation/invitation.component';
+import { LocationComponent } from './location/location.component';
 
 
 const routes: Routes = [
-  {path: '', component: InvitationComponent}
+  {path: '', component: InvitationComponent},
+  {path: 'location', component: LocationComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+declare var Hammer: any;
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InvitationComponent } from './invitation/invitation.component';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { LocationComponent } from './location/location.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides: any = {
+    pan: { direction: Hammer.DIRECTION_All },
+    swipe: { direction: Hammer.DIRECTION_VERTICAL }
+  };
+
+  buildHammer(element: HTMLElement) {
+    const mc = new Hammer(element, {
+      touchAction: 'auto',
+          inputClass: Hammer.SUPPORT_POINTER_EVENTS ? Hammer.PointerEventInput : Hammer.TouchInput,
+          recognizers: [
+            [Hammer.Swipe, {
+              direction: Hammer.DIRECTION_HORIZONTAL
+            }]
+          ]
+    });
+    return mc;
+  }
+}
 
 @NgModule({
   declarations: [
     AppComponent,
-    InvitationComponent
+    InvitationComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    CollapseModule.forRoot()
+    MDBBootstrapModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: MyHammerConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
