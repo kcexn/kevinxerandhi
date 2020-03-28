4d81d591098d4b627391d79b0cbe98af9e2a17e6
Kevin <068ant@gmail.com>
2020-03-28 22:51:38 +1100

    Still trying to fix the navbar padding issue on firefox

--- src/app/services/guest-rsvp.service.ts
+++ src/app/services/guest-rsvp.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import 'firebase/firestore';
 
 import { GuestRSVP } from '../interfaces/interfaces';
 
